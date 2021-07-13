f791fd2506d8f032a97664bb907f73454749328c
Leon <LeoninLyon@gmail.com>
2021-07-13 20:56:20 +0800

    🔨Fix errors

--- Tasks/qlrepo/extra.sh
+++ Tasks/qlrepo/extra.sh
@@ -81,19 +81,19 @@ ql repo https://github.com/shufflewzc/faker2.git "jd_|jx_|getJDCookie" "activity
 # 适用于柠檬胖虎代维护 lxk0301 仓库，宠汪汪二代目和宠汪汪兑换，只支持国内机。
 apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && cd scripts && npm install canvas --build-from-source
 # 适用于 JDHelloWorld 和 he1pu 的宠汪汪二代目和宠汪汪兑奖品二代目
-cd scripts && npm install png-js -S 
+npm install png-js -S
 # 财富岛依赖安装命令
 ## 安装 date-fns
 npm install date-fns -S
 ## 安装 axios
 npm install axios -S
 ## 安装 crypto-js
-npm install -g crypto-js -S
+npm install crypto-js -S
 ## 安装 ts-md5
-npm install -g ts-md5 -S
+npm install ts-md5 -S
 ## 安装 tslib
-npm install -g tslib -S
+npm install tslib -S
 ## 安装 @types/node
-npm install -g @types/node -S
+npm install @types/node -S
 ## Python 3 安装 requests
 pip3 install requests
