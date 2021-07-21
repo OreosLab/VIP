3c193a2872088031d520ae64312e47cbed826bed
Leon <62660101+Oreomeow@users.noreply.github.com>
2021-07-21 23:24:35 +0800

    🍒Modify & Add

--- Conf/Qinglong/config.sample.sh
+++ Conf/Qinglong/config.sample.sh
@@ -111,21 +111,24 @@ export GOBOT_TOKEN=""
 export GOBOT_QQ=""
 
 ## 10. 临时屏蔽某个Cookie
-## 如果某些Cookie已经失效了，但暂时还没法更新，可以使用此功能在不删除该Cookie和重新修改Cookie编号的前提下，临时屏蔽掉某些编号的Cookie
+## 如果某些 Cookie 已经失效了，但暂时还没法更新，可以使用此功能在不删除该Cookie和重新修改Cookie编号的前提下，临时屏蔽掉某些编号的Cookie
 ## 多个Cookie编号以半角的空格分隔，两侧一对半角双引号，使用此功能后，在运行js脚本时账户编号将发生变化
-## 举例1：TempBlockCookie="2"    临时屏蔽掉Cookie2
-## 举例2：TempBlockCookie="2 4"  临时屏蔽掉Cookie2和Cookie4
+## 举例1：TempBlockCookie="2"    临时屏蔽掉 Cookie2
+## 举例2：TempBlockCookie="2 4"  临时屏蔽掉 Cookie2 和 Cookie4
 
-## 如果只是想要屏蔽某个账号不参加某些活动，可以参考下面 case 这个命令的例子来控制
+## 如果只是想要屏蔽某个 Cookie 不参加某些活动，可以参考下面 case 这个命令的例子来控制
 ## case $1 in
 ##     *jd_fruit*)                            # 东东农场活动脚本关键词
-##         TempBlockCookie="5"                # 账号5不玩东东农场
+##         TempBlockCookie="5"                # Cookie5 不玩东东农场
 ##         ;;
 ##     *jd_dreamFactory* | *jd_jdfactory*)    # 京喜工厂和东东工厂的活动脚本关键词
-##         TempBlockCookie="2"                # 账号2不玩京喜工厂和东东工厂
+##         TempBlockCookie="2"                # Cookie2 不玩京喜工厂和东东工厂
 ##         ;;
 ##     *jd_jdzz* | *jd_joy*)                  # 京喜赚赚和宠汪汪的活动脚本关键词
-##         TempBlockCookie="3 6"              # 账号3、账号6不玩京东赚赚和宠汪汪
+##         TempBlockCookie="3 6"              # Cookie3 、Cookie6 不玩京东赚赚和宠汪汪
+##         ;;
+##     *)                                     # 其他活动
+##         TempBlockCookie=""                 # 默认为空值，表示其他帐号参加全部活动。填写帐号序号表示临时屏蔽指定 Cookie 参加其他活动
 ##         ;;
 ## esac
 case $1 in
@@ -136,6 +139,9 @@ case $1 in
         TempBlockCookie=""
         ;;
     *jd_jdzz* | *jd_joy*)
+        TempBlockCookie=""
+        ;;
+    *)
         TempBlockCookie=""
         ;;
 esac
@@ -274,13 +280,6 @@ export SUPER_RAIN_RRA=""
 ## 半点京豆雨RRA
 export HALF_RAIN_RRA=""
 
-# JDHelloWorld 部分环境变量
-## 宠汪汪二代目
-### 默认80，10、20、40、80可选
-export feedNum="80"
-### 默认双人跑
-export JD_JOY_teamLevel="2"
-
 # 柠檬（胖虎部分环境变量）
 ## 1、京喜工厂抢茅台
 ### 自定义商品变量
@@ -380,18 +379,22 @@ export dd_thread="30"
 export starttime="23:59:59.00000000"
 ### 结束时间
 export endtime="00:00:30.00000000"
+## 6、京喜工厂开团助力 for Python
+### 支持指定账号开团，跑1次脚本默认开3次团，如未指定账号默认给账号一开团。
+### 变量ENV 指定开团账号。可填用户名 或 pt_pin 的值。示例：export jxgc_kaituan="用户1&用户2"
+export jxgc_kaituan="$(echo $JD_COOKIE | sed "s/&/ /g; s/\S*pt_pin=\(\S\+\)\S*;/\1/g; s/ /\&/g;" | awk 'BEGIN{for(i=0;i<10;i++)hex[i]=i;hex["A"]=hex["a"]=10;hex["B"]=hex["b"]=11;hex["C"]=hex["c"]=12;hex["D"]=hex["d"]=13;hex["E"]=hex["e"]=14;hex["F"]=hex["f"]=15;}{gsub(/\+/," ");i=$0;while(match(i,/%../)){;if(RSTART>1);printf"%s",substr(i,1,RSTART-1);printf"%c",hex[substr(i,RSTART+1,1)]*16+hex[substr(i,RSTART+2,1)];i=substr(i,RSTART+RLENGTH);}print i;}')"
 
 # Wenmoux 部分环境变量 
 ## 1、QQ星系牧场自动兑换20豆
 export Cowexchange="true"
-## 欧洲狂欢杯兑换兑换豆子，填38豆子，填39e卡
+## 2、欧洲狂欢杯兑换兑换豆子，填38豆子，填39e卡
 export Cupexid="39"
-## 10秒阅读
-## 填写自己CK
+## 3、10秒阅读
+### 填写自己CK
 export Readck=""
-## 填写自己设备UA
+### 填写自己设备UA
 export Read10UA=""
-## 填true推送消息，默认不推送
+### 填true推送消息，默认不推送
 export jrpush=""
 
 # smiek2221 环境变量
@@ -419,7 +422,12 @@ export olympicgames_inviteId=""
 export exchangeAccounts="$(echo $JD_COOKIE | sed "s/&/ /g; s/\S*pt_pin=\(\S\+\);\S*/\1@2/g; s/ /&/g;")"           ##兑2元现金
 
 # JDHelloWorld 环境变量
-## 1、新版京喜财富岛提现
+## 1、宠汪汪二代目
+### 默认80，10、20、40、80可选
+export feedNum="80"
+### 默认双人跑
+export JD_JOY_teamLevel="2"
+## 2、新版京喜财富岛提现
 ### 提现金额，可选0.1 0.5 1 2 10
 export CFD_CASHOUT_MONEY=10
 ### token，顺序、数量必须与cookie一致。抓包地址：https://m.jingxi.com/jxbfd/user/ExchangePrize
