d3f2fa1650e79ace7c26704c2c027f247ac9b1de
Leon <62660101+Oreomeow@users.noreply.github.com>
2021-07-23 20:25:58 +0800

    🌱Upsample.sh

---
+++ Scripts/sh/Upsample.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker exec -it qinglong /bin/sh -c "cd /ql/config
+curl -sL https://git.io/config.sh > /ql/config/config.sample.sh
+cd /ql/sample && rm config.sample.sh
+cp -rf /ql/config/config.sample.sh /ql/sample"
