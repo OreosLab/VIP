cc26ce437ba4d52b4d6006ac7d39ada47f8f57a1
Leon <62660101+Oreomeow@users.noreply.github.com>
2021-07-24 10:44:14 +0800

    🍭

--- Scripts/sh/Update.sh
+++ Scripts/sh/Update.sh
@@ -27,7 +27,7 @@ mv -b $code_raw_path $dir_config
 sed -i "s/\$repo4/\$repo${repoNum}/g" $code_config_path
 sed -i '/## 填 2 使用“随机顺序互助模板”，本套脚本内账号间随机顺序助力，每次生成的顺序都不一致。/{:a;n;s/1/0/g;/## 定义指定活动采用指定的互助模板。/!ba}' $code_config_path
 sed -i '/## 不按示例填写可能引发报错。/{:a;n;s/0/1/g;/## 屏蔽模式/!ba}' $code_config_path
-sed -i 's/BreakHelpNum=\"4 9-14 15~18 19_21\"  ## 屏蔽账号序号或序号区间/BreakHelpNum=\"11-47\"  ## 屏蔽账号序号或序号区间/g" $code_config_path
+sed -i "s/BreakHelpNum=\"4 9-14 15~18 19_21\"  ## 屏蔽账号序号或序号区间/BreakHelpNum=\"11-47\"  ## 屏蔽账号序号或序号区间/g" $code_config_path
 
 curl -sL https://git.io/task_before.sh > $task_before_raw_path
 # mv -b $task_before_raw_path $dir_config
