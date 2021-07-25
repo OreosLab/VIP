20d4d812521f1cdd92b0527b254c14ed083f31a0
Leon <62660101+Oreomeow@users.noreply.github.com>
2021-07-25 13:27:03 +0800

    🍭Test

--- Scripts/sh/DlHelper.sh
+++ Scripts/sh/DlHelper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+dir_sync=/root/Help/互助研究院\(1597522865\)/2021年07月
 
 
 (
@@ -9,3 +10,8 @@
   echo "msg DlHelper /start https://t.me/update_help" 
   echo "safe_quit"
 ) | docker exec -i telegram-cli telegram-cli -N
+
+
+find $dir_sync -name "*code\.sh" -exec mv {} $dir_sync/code\.sh \;
+find $dir_sync -name "*task_before\.sh" -exec mv {} $dir_sync/task_before\.sh \;
+find $dir_sync -name "*config_sample\.sh" -exec mv {} $dir_sync/config_sample\.sh \;
