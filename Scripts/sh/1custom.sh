75c04caef0668ce780ebb9ada266a0de40f857d5
Leon <62660101+Oreomeow@users.noreply.github.com>
2021-07-28 13:21:37 +0800

    🔨Fix

--- Scripts/sh/1custom.sh
+++ Scripts/sh/1custom.sh
@@ -150,7 +150,7 @@ fi
 if [ "$(grep -c "config.sample.sh" /ql/config/crontab.list)" != 0 ] && [ "${Rsample}" = 'y' -o "${Rall}" = 1 ]; then
     echo "您的任务列表中已存在 task 自动更新模板"
 elif [ "$(grep -c "config.sample.sh" /ql/config/crontab.list)" = 0 ] && [ "${Rsample}" = 'y' -o "${Rall}" = 1 ]; then
-    echo "开始添加 task ql bot"
+    echo "开始添加 task curl config.sample.sh"
     # 获取token
     token=$(cat /ql/config/auth.json | jq --raw-output .token)
     curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"自动更新模板","command":"curl -L https://git.io/config.sh -o /ql/sample/config.sample.sh && cp -rf /ql/sample/config.sample.sh /ql/config","schedule":"45 6,18 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1627380635389'
