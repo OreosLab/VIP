d7167eb3d0b4e61add67e5a0fd0ef6c12536f625
Leon <62660101+Oreomeow@users.noreply.github.com>
2021-08-09 17:32:44 +0800

    🔨Modify

--- Scripts/py/EUserv_tsf.py
+++ Scripts/py/EUserv_tsf.py
@@ -25,12 +25,12 @@ PUSH_PLUS_TOKEN = os.environ.get('PUSH_PLUS_TOKEN')
 # Telegram Bot Push https://core.telegram.org/bots/api#authorizing-your-bot
 TG_BOT_TOKEN = os.environ.get('TG_BOT_TOKEN')  # 通过 @BotFather 申请获得，示例：1077xxx4424:AAFjv0FcqxxxxxxgEMGfi22B4yh15R5uw
 TG_USER_ID = os.environ.get('TG_USER_ID')  # 用户、群组或频道 ID，示例：129xxx206
-TG_API_HOST = 'api.telegram.org'  # 自建 API 反代地址，供网络环境无法访问时使用，网络正常则保持默认
+TG_API_HOST = os.environ.get('TG_API_HOST', 'api.telegram.org')  # 自建 API 反代地址，供网络环境无法访问时使用，网络正常则保持默认
 
 # wecomchan https://github.com/easychen/wecomchan
 WECOMCHAN_DOMAIN = os.environ.get('WECOMCHAN_DOMAIN')  # http(s)://example.com/
 WECOMCHAN_SEND_KEY = os.environ.get('WECOMCHAN_SEND_KEY')
-WECOMCHAN_TO_USER = '@all'  # 默认全部推送, 对个别人推送可用 User1|User2
+WECOMCHAN_TO_USER = os.environ.get('WECOMCHAN_TO_USER', '@all')  # 默认全部推送, 对个别人推送可用 User1|User2
 # 变量命名使用全部小写的方式，可以使用下划线。
 desp = ''  # 不用动
 
