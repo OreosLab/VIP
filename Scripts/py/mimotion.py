49c02d9fa461580d05a6ab4c12b407371534b2e5
Leon <LeoninLyon@gmail.com>
2021-08-10 17:37:10 +0800

    🔨

--- Scripts/py/mimotion.py
+++ Scripts/py/mimotion.py
@@ -5,11 +5,12 @@
 Author: Squaregentleman
 Modifier: Oreo
 Date: Tue Aug 10 08:24:30 UTC 2021
+建议cron: 30 19 * * *  python3 EUserv_extend.py
 ------------
 环境变量说明
 MI_USER: 账号      仅支持手机号，多账号用 # 分隔
 MI_PWD: 密码       多账号用 # 分隔，且与账号一一对应
-STEP: 步数         空则为 18000-25000 之间随机，自定义示例: 18763 或 19000-24000
+STEP: 步数         0 则为 18000-25000 之间随机，自定义示例: 18763 或 19000-24000
 PMODE: 推送模式 || PKEY: 具体推送格式填写（不带 [ ]，请用具体的值代替)
 wx                [Server 酱: skey]
 nwx               [新 Server 酱: skey]
