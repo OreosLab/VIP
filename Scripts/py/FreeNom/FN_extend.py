bbdef198a9a3da39de554fecb1628e5afb9e78f9
Leon <LeoninLyon@gmail.com>
2021-08-11 21:03:05 +0800

    🔨Fix bug

--- Scripts/py/FreeNom/FN_extend.py
+++ Scripts/py/FreeNom/FN_extend.py
@@ -10,8 +10,8 @@ FN_ID: Freenom 用户名           1234567890@gmail.com
 FN_PW: Freenom 密码             12345678
 MAIL_USER: 发件人邮箱用户名      address@vip.qq.com 或 123456@qq.com
 MAIL_ADDRESS: 发件人邮箱地址     address@vip.qq.com 或 123456@qq.com
-MAIL_PW: 发件人邮箱密码          87654321
-* MAIL_HOST: 发件人邮箱服务器    smt.qq.com 不填默认为这个
+MAIL_PW: 发件人邮箱授权码        xxxxxxxxxxxxxxxx 看下方链接
+* MAIL_HOST: 发件人邮箱服务器    smtp.qq.com 不填默认为这个
 * MAIL_PORT: 邮箱服务器端口      465 不填默认为这个
 MAIL_TO: 收件人邮箱可与发件人相同 address@vip.qq.com 或 123456@qq.com   
 
