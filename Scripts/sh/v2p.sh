a83d6ef71d5e0946a06b18e421591c7ebcd86ef0
Leon <62660101+Oreomeow@users.noreply.github.com>
2021-08-13 15:20:58 +0800

    🔨fix

--- Scripts/sh/v2p.sh
+++ Scripts/sh/v2p.sh
@@ -172,9 +172,9 @@ opt
 read net
 if [ "$net" = "1" ]; then
     NETWORK="host"
-    CHANGE_NETWORK="--network $NETWORK"
+    CHANGE_NETWORK="--net $NETWORK"
 else
-    CHANGE_NETWORK="--network $NETWORK"
+    CHANGE_NETWORK="--net $NETWORK"
 fi
 
 inp "是否修改端口[默认 8100|8101|8102]：\n1) 修改\n2) 不修改[默认]"
