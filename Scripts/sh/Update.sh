33976c827ddf4e3f3d5cdafade36824923c117d6
Leon <LeoninLyon@gmail.com>
2021-08-16 18:43:03 +0800

    🔨Update.sh

--- Scripts/sh/Update.sh
+++ Scripts/sh/Update.sh
@@ -17,8 +17,11 @@ code_config_path=$dir_config/code.sh
 task_before_raw_path=$dir_raw/task_before.sh
 task_before_config_path=$dir_config/task_before.sh
 
+GithubProxyUrl=""
 TG_BOT_TOKEN=""
 TG_USER_ID=""
+openCardBean="10"
+
 CollectedRepo="4"
 OtherRepo=""
 repoNum="4"
@@ -30,8 +33,10 @@ BreakHelpNum="BreakHelpNum=\"11-1000\""
 
 curl -sL https://git.io/config.sh > $config_raw_path
 mv -b $config_raw_path $dir_config
-sed -i "s/TG_BOT_TOKEN=\"\"/TG_BOT_TOKEN=\"$TG_BOT_TOKEN\"/g" $config_config_path
-sed -i "s/TG_USER_ID=\"\"/TG_USER_ID=\"$TG_USER_ID\"/g" $config_config_path
+sed -ri "s/GithubProxyUrl=\"https\:\/\/ghproxy.com\/\"/GithubProxyUrl=\"${GithubProxyUrl}\"/g" $config_config_path
+sed -i "s/TG_BOT_TOKEN=\"\"/TG_BOT_TOKEN=\"${TG_BOT_TOKEN}\"/g" $config_config_path
+sed -i "s/TG_USER_ID=\"\"/TG_USER_ID=\"${TG_USER_ID}\"/g" $config_config_path
+sed -i "s/openCardBean=\"30\"/openCardBean=\"${openCardBean}\"/g" $config_config_path
 
 curl -sL https://git.io/extra.sh > $extra_raw_path
 mv -b $extra_raw_path $dir_config
@@ -41,25 +46,30 @@ sed -i "s/OtherRepo=()/OtherRepo=(${OtherRepo})/g" $extra_config_path
 curl -sL https://git.io/code.sh > $code_raw_path
 mv -b $code_raw_path $dir_config
 
-sed -i "s/repo=\$repo6/repo=\$repo${repoNum}/g" $code_config_path
+sed -i "s/repo=\$repo4/repo=\$repo${repoNum}/g" $code_config_path
 sed -i "/^HelpType=/c${HelpType}" $code_config_path
 sed -i "/^BreakHelpType=/c${BreakHelpType}" $code_config_path
 sed -i "/^BreakHelpNum=/c${BreakHelpNum}" $code_config_path
 
 curl -sL https://git.io/task_before.sh > $task_before_raw_path
-# mv -b $task_before_raw_path $dir_config
+mv -b $task_before_raw_path $dir_config
 
-c=1000000
-for r in {1..3}; do
-	p=`expr $c - $r`
-	sed -ri "s/\"position\"\:${p}/regular${r}/" $file_db
-done
-for line in {1..100}; do
-	sed -ri "${line}s/(\"position\"\:)[^,]*/\"position\"\:${RANDOM}/" $file_db
-done
-for r in {1..3}; do
-	p=`expr $c - $r`
-	sed -ri "s/regular${r}/\"position\"\:${p}/" $file_db
-done
+random_cookie(){
+  c=1000000
+  for r in {1..3}; do
+      p=`expr $c - $r`
+      sed -ri "s/\"position\"\:${p}/regular${r}/" $file_db
+  done
+  for line in {1..100}; do
+      sed -ri "${line}s/(\"position\"\:)[^,]*/\"position\"\:${RANDOM}/" $file_db
+  done
+  for r in {1..3}; do
+      p=`expr $c - $r`
+      sed -ri "s/regular${r}/\"position\"\:${p}/" $file_db
+  done
+  ql update
+}
 
-ql update
+if [ $(date "+%H") -eq 18 ]; then
+    random_cookie
+fi
