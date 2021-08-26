ea5bb41398fcd4b83798824672aba8aa396f05bc
Leon <LeoninLyon@gmail.com>
2021-08-26 12:56:34 +0800

    🌱for V2P

---
+++ Scripts/js/Wechat-Timed-Message.js
@@ -0,0 +1,18 @@
+console.log("⏳ 开始发送微信消息")
+$exec('python3 Wechat-Timed-Message.py', {
+  cwd: './script/Shell',
+  timeout: 0,
+  env: {
+    SCKEY: $store.get('SCKEY', 'string'),
+    PUSH_PLUS_TOKEN: $store.get('PUSH_PLUS_TOKEN', 'string'),
+    PUSH_PLUS_TOPIC: $store.get('PUSH_PLUS_TOPIC', 'string'),
+    TITLE: $store.get('TITLE', 'string'),
+    MSG: $store.get('MSG', 'string'),
+    IMAGE: $store.get('IMAGE', 'string'),
+    CONTENT: $store.get('CONTENT', 'string'),
+    QYWX_AM: $store.get('QYWX_AM', 'string'),  
+  },
+  cb(data, error) {
+    error ? console.error(error) : console.log(data)
+  }
+})
